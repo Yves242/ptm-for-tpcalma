85b7c7467eb12ee011aa6475a65dd4188af935b7
yvbal <70612985+Yves242@users.noreply.github.com>
2024-02-27 23:35:09 +0800

    Created review for variables and if-then conditions

---
+++ codes/0. variables/variables.py
@@ -0,0 +1,35 @@
+''' This code reviews variables and if-else conditions. '''
+
+# variable declarations
+num_variable = 10
+    
+    
+# start of executing 
+
+
+# =============== PART 1 ================= #
+
+
+# print case no. 1
+print(num_variable)    # what's the value?
+
+
+
+# =============== PART 1 ================= #
+
+
+
+# add 20 to num_variable
+num_variable += 20
+
+# print case no. 2
+print(num_variable)    # what's the value?
+
+
+# ===== add 100 to num_variable =======
+num_variable += 100
+
+# print case no. 3
+print(num_variable)    # what's the value?
+
+
